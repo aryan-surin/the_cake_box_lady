c6fb517930656a89e9ce6542b6d9e8b180ec3070
aryan-surin <aryansurin21@gmail.com>
2026-05-12 18:51:51 +0530

    updated the png and favicon as per the cake box lady.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export const metadata: Metadata = {
         url: '/icon.svg',
         type: 'image/svg+xml',
       },
+        {
+          url: '/favicon.ico',
+        },
     ],
     apple: '/apple-icon.png',
   },
